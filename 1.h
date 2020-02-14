8cee25d1a586cc7d845a1b44769a6e42424a2698
SizovV <48352454+SizovV@users.noreply.github.com>
2020-02-14 15:00:26 +0300

    Add files via upload

---
+++ 1.h
@@ -0,0 +1,11 @@
+#!/usr/bin/gnuplot -persist
+
+set terminal postscript eps enhanced color font 'Comic-Sans,14'
+set output "Abs_high.eps"
+set xlabel "Time, s" font 'Comic-Sans,14'
+set ylabel "Temperature, C" font 'Comic-Sans,14'
+set xtics border in scale 1,0.5 nomirror norotate autojustify
+set ytics border in scale 1,0.5 nomirror norotate autojustify
+set key left reverse
+
+plot "./Desktop/testlaba" u 0:1 w l title ""
